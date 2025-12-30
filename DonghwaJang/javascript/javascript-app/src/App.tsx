5ae9258de70ec1e5f0eebaa2ea321812b2d23eef
dongsu2039-tech <dongsu2039@gmail.com>
2025-12-30 13:38:46 +0900

    feat: [장동화] Math 기능 및 switch 키워드 활용 [PMIF-x]

--- DonghwaJang/javascript/javascript-app/src/App.tsx
+++ DonghwaJang/javascript/javascript-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
+import { ControlFlowSwitch } from './control_flow/ControlFlowIswitch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataType'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { CalculationExample } from './math_operation/CalculationExample'
@@ -16,6 +17,7 @@ function App() {
         <LogicalOperationExample/>
         <ControlFlowIf/>
         <FirstProblem/>
+        <ControlFlowSwitch/>
       
       </div>
     </>
