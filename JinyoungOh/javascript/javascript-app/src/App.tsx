d0a7e8da3df05bfe82c7ccf938626189bbecc90e
5jin0 <jinyoung120@naver.com>
2025-12-31 16:29:01 +0900

    feat: [오진영] 배열 splice 연산 [PMIF-x]

--- JinyoungOh/javascript/javascript-app/src/App.tsx
+++ JinyoungOh/javascript/javascript-app/src/App.tsx
@@ -13,6 +13,7 @@ import { MapExample } from './map/MapExample'
 import { MapReduceExample } from './map/MapReduceExample'
 import { MapFilterExample } from './map/MapFilterExample'
 import { ArraySliceExample } from './array/ArraySliceExample'
+import { ArraySpliceExample } from './array/ArraySpliceExample'
 
 function App() {
 
@@ -33,6 +34,7 @@ function App() {
       <MapReduceExample/>
       <MapFilterExample/>
       <ArraySliceExample/>
+      <ArraySpliceExample/>
       </div>
     </>
   )
